81737864a348f283ca069fd8be6e380728238bc9
Matthew Lemon <lemon@matthewlemon.com>
2017-04-07 14:36:00 +0100

    another setup.py change

--- setup.py
+++ setup.py
@@ -1,11 +1,7 @@
 from setuptools import setup
 
 requirements = [
-    'pyqt5',
-    'openpyxl',
-    'python-dateutil',
-    'sqlalchemy',
-    'appdirs'
+    'pyqt5', 'openpyxl', 'python-dateutil', 'sqlalchemy', 'appdirs'
 ]
 
 test_requirements = [
@@ -24,12 +20,14 @@ setup(
     author="Matthew Lemon",
     author_email='matt@matthewlemon.com',
     url='https://github.com/hammerheadlemon/xldigest',
-    packages=['xldigest', 'xldigest.images',
-              'xldigest.tests'],
+    packages=[
+        'xldigest', 'xldigest.process', 'xldigest.process',
+        'xldigest.database', 'xldigest.widgets', 'xldigest.analysis',
+        'xldigest.tests'
+    ],
     entry_points={
-        'console_scripts': [
-            'xldigest-populate = xldigest.database.populate:main'
-        ]
+        'console_scripts':
+        ['xldigest-populate = xldigest.database.populate:main']
     },
     install_requires=requirements,
     zip_safe=False,
@@ -40,5 +38,4 @@ setup(
         'Programming Language :: Python :: 3.5',
     ],
     test_suite='tests',
-    tests_require=test_requirements
-)
+    tests_require=test_requirements)
