9e0f5627eaf4eef5deda99e1114861586d8f9aae
Matthew Lemon <lemon@matthewlemon.com>
2017-04-24 16:41:50 +0100

    using a tableview now for showing status of selected files for import

--- xldigest/widgets/importreturns.py
+++ xldigest/widgets/importreturns.py
@@ -20,6 +20,12 @@ class ImportReturns(QtWidgets.QWidget, Ui_ImportManager):
         self.comboSeriesItem.activated.connect(self._series_item_select)
         self.selectedCountLabel.setText("")
 
+    def _make_model_data_list(self, *args):
+        """
+        Tiny helper method.
+        """
+        return list(args)
+
     def get_return_source_files(self):
         print("Launching dialog to choose source files for returns")
         self.import_returns_dir_dialog = QtWidgets.QFileDialog()
@@ -30,9 +36,13 @@ class ImportReturns(QtWidgets.QWidget, Ui_ImportManager):
         if self.import_returns_dir_dialog.exec():
             self.selected_files = self.import_returns_dir_dialog.selectedFiles()
             print(self.selected_files)
-            self.selectedFilesWidget.clear()
-            for s in self.selected_files:
-                self.selectedFilesWidget.addItem(QtWidgets.QListWidgetItem(s))
+            l = []
+            for sfile in self.selected_files:
+                l.append(self._make_model_data_list(sfile, "bollocks"))
+            self.model_selected_returns = SelectedFilesModel(l)
+            self.selectedFilesWidget.setModel(self.model_selected_returns)
+            self.selectedFilesWidget.horizontalHeader().setStretchLastSection(True)
+            print(l)
         self.selectedCountLabel.setText("{} files selected".format(len(self.selected_files)))
 
     def _portfolio_select(self, index):
@@ -76,3 +86,40 @@ class ImportReturns(QtWidgets.QWidget, Ui_ImportManager):
             item_text = QtGui.QStandardItem(item)
             model.appendRow(item_text)
         return model
+
+
+class SelectedFilesModel(QtCore.QAbstractTableModel):
+    """
+    Status of selected return files. Needed so that we can match up what we're
+    intending to import with whats in the database. We don't want duplicates.
+    """
+    def __init__(self, data_in, parent=None, *args):
+        super().__init__(parent)
+        self.data_in = data_in
+
+    def rowCount(self, parent=QtCore.QModelIndex()):
+        return len(self.data_in)
+
+    def columnCount(self, parent=QtCore.QModelIndex()):
+        return len(self.data_in[0])
+
+    def headerData(self, section, orientation, role):
+        headers = [
+            "File Name",
+            "Status"
+        ]
+        if role == QtCore.Qt.DisplayRole:
+            if orientation == QtCore.Qt.Horizontal:
+                return headers[section]
+            else:
+                return section + 1
+
+    def flags(self, index):
+        return QtCore.Qt.ItemIsEnabled
+
+    def data(self, index, role):
+        if index.isValid() and role == QtCore.Qt.DisplayRole:
+            row = index.row()
+            col = index.column()
+            value = self.data_in[row][col]
+            return value
