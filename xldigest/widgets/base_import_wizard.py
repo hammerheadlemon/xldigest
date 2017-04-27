d6156930ee763f398c21e56d37c4fd87691154fb
Matthew Lemon <lemon@matthewlemon.com>
2017-04-27 11:54:23 +0100

    reconfigured the ui file a bit

--- xldigest/widgets/base_import_wizard.py
+++ xldigest/widgets/base_import_wizard.py
@@ -7,3 +7,9 @@ class BaseImportWizard(QtWidgets.QWizard, Ui_baseWizard):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setupUi(self)
+        self.create_portfolio_button.clicked.connect(self._create_portfolio_diag)
+
+    def _create_portfolio_diag(self):
+        diag = QtWidgets.QDialog(parent=self)
+        if diag.exec_():
+            self.captured = True
