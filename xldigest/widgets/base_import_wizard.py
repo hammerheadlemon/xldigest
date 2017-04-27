6eba4091577bb8cceada532ccddf5fcf426548bb
Matthew Lemon <lemon@matthewlemon.com>
2017-04-27 15:58:00 +0100

    can now add a series to the widget

--- xldigest/widgets/base_import_wizard.py
+++ xldigest/widgets/base_import_wizard.py
@@ -1,19 +1,24 @@
 from PyQt5 import QtWidgets
 
-from xldigest.widgets.base_import_wizard_ui import Ui_baseWizard
-from xldigest.widgets.dialogs import AddPortfolioDialog, AddProjectDialog
+from xldigest.widgets.base_import_wizard_ui import Ui_base_import_wizard
+from xldigest.widgets.dialogs import (AddPortfolioDialog, AddProjectDialog,
+                                      AddSeriesDialog)
 
 
-class BaseImportWizard(QtWidgets.QWizard, Ui_baseWizard):
+class BaseImportWizard(QtWidgets.QWizard, Ui_base_import_wizard):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setupUi(self)
         self.projects = []
         self.portfolio = None
+        self.series = None
+        self.series_items = []
         self.create_portfolio_button.clicked.connect(
             self._create_portfolio_diag)
         self.add_project_button.clicked.connect(self._add_project_diag)
+        self.create_series_button.clicked.connect(self._create_series_diag)
         self.portfolio_added_label.setEnabled(False)
+        self.series_added_label.setEnabled(False)
         self.added_projects_table.setColumnCount(1)
         self.added_projects_table.setHorizontalHeaderItem(
             0, QtWidgets.QTableWidgetItem("Projects Added"))
@@ -37,3 +42,11 @@ class BaseImportWizard(QtWidgets.QWizard, Ui_baseWizard):
             self.added_projects_table.insertRow(rows)
             p_add = QtWidgets.QTableWidgetItem(new_project_name)
             self.added_projects_table.setItem(rows, 0, p_add)
+
+    def _create_series_diag(self):
+        diag = AddSeriesDialog()
+        if diag.exec_():
+            self.series = diag.name_lineEdit.text()
+            self.series_added_label.setEnabled(True)
+            self.series_added_label.setText(self.series)
+            print(diag.name_lineEdit.text())
--- xldigest/widgets/dialogs.py
+++ xldigest/widgets/dialogs.py
@@ -32,3 +32,18 @@ class AddProjectDialog(QtWidgets.QDialog):
         buttonBox.accepted.connect(self.accept)
         buttonBox.rejected.connect(self.reject)
 
+
+class AddSeriesDialog(QtWidgets.QDialog):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        name_label = QtWidgets.QLabel("Series Name")
+        self.name_lineEdit = QtWidgets.QLineEdit("Series Name")
+        buttonBox = QtWidgets.QDialogButtonBox(QtWidgets.QDialogButtonBox.Ok |
+                                               QtWidgets.QDialogButtonBox.Cancel)
+        grid = QtWidgets.QGridLayout()
+        grid.addWidget(name_label, 0, 0)
+        grid.addWidget(self.name_lineEdit, 0, 1)
+        grid.addWidget(buttonBox, 2, 0, 1, 2)
+        self.setLayout(grid)
+        buttonBox.accepted.connect(self.accept)
+        buttonBox.rejected.connect(self.reject)
