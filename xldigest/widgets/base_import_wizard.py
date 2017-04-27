a0eac75906d9128f23a5f53e4a680ca28b01c599
Matthew Lemon <lemon@matthewlemon.com>
2017-04-27 14:33:44 +0100

    making progress on dialogues to add portfolio and project

--- xldigest/widgets/base_import_wizard.py
+++ xldigest/widgets/base_import_wizard.py
@@ -1,17 +1,30 @@
 from PyQt5 import QtWidgets
 
 from xldigest.widgets.base_import_wizard_ui import Ui_baseWizard
-from xldigest.widgets.add_portfolio_diag import AddPortfolioDialog
+from xldigest.widgets.dialogs import AddPortfolioDialog, AddProjectDialog
 
 
 class BaseImportWizard(QtWidgets.QWizard, Ui_baseWizard):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setupUi(self)
+        self.projects = []
+        self.portfolio = None
         self.create_portfolio_button.clicked.connect(self._create_portfolio_diag)
+        self.add_project_button.clicked.connect(self._add_project_diag)
+        self.portfolio_added_label.setEnabled(False)
 
     def _create_portfolio_diag(self):
         diag = AddPortfolioDialog()
         if diag.exec_():
-            self.new_portfolio_name = diag.name_lineEdit.text()
-            print(self.new_portfolio_name)
+            self.portfolio = diag.name_lineEdit.text()
+            self.portfolio_added_label.setEnabled(True)
+            self.portfolio_added_label.setText(self.portfolio)
+            print(diag.name_lineEdit.text())
+
+    def _add_project_diag(self):
+        diag = AddProjectDialog()
+        if diag.exec_():
+            new_project_name = diag.name_lineEdit.text()
+            print(new_project_name)
+            self.projects.append(new_project_name)
--- xldigest/widgets/dialogs.py
+++ xldigest/widgets/dialogs.py
@@ -15,3 +15,20 @@ class AddPortfolioDialog(QtWidgets.QDialog):
         self.setLayout(grid)
         buttonBox.accepted.connect(self.accept)
         buttonBox.rejected.connect(self.reject)
+
+
+class AddProjectDialog(QtWidgets.QDialog):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        name_label = QtWidgets.QLabel("Project Name")
+        self.name_lineEdit = QtWidgets.QLineEdit("Project Name")
+        buttonBox = QtWidgets.QDialogButtonBox(QtWidgets.QDialogButtonBox.Ok |
+                                               QtWidgets.QDialogButtonBox.Cancel)
+        grid = QtWidgets.QGridLayout()
+        grid.addWidget(name_label, 0, 0)
+        grid.addWidget(self.name_lineEdit, 0, 1)
+        grid.addWidget(buttonBox, 2, 0, 1, 2)
+        self.setLayout(grid)
+        buttonBox.accepted.connect(self.accept)
+        buttonBox.rejected.connect(self.reject)
+
