bfa2a02ef5cac7bc65298bfedeba45b249b7e791
Matthew Lemon <lemon@matthewlemon.com>
2017-04-27 20:35:43 +0100

    added some nice icons and a toolbar to the main window

--- xldigest/main.py
+++ xldigest/main.py
@@ -7,7 +7,7 @@
 # WARNING! All changes made in this file will be lost!
 import sys
 
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtGui, QtCore
 from xldigest.widgets.main_ui import Ui_MainXldigestWindow
 from xldigest.widgets.template_manager_window import TemplateManagerWindow
 
@@ -16,6 +16,27 @@ class XldigestMainWindow(QtWidgets.QMainWindow, Ui_MainXldigestWindow):
     def __init__(self, parent=None):
         super(XldigestMainWindow, self).__init__(parent)
         self.setupUi(self)
+        self.toolBar = QtWidgets.QToolBar("Main Toolbar", parent=self)
+        self.toolBar.setIconSize(QtCore.QSize(64, 64))
+        self.toolBar.setMovable(False)
+        self.toolBar.setToolButtonStyle(QtCore.Qt.ToolButtonTextUnderIcon)
+        self.addToolBar(QtCore.Qt.TopToolBarArea, self.toolBar)
+
+        self.overviewIcon = QtGui.QPixmap('/home/lemon/Downloads/icons_tmp/circle-icons/one-color/png/64px/play.png')
+        self.toolBar.addAction(QtGui.QIcon(self.overviewIcon), "Overview")
+
+        self.datamapIcon = QtGui.QPixmap('/home/lemon/Downloads/icons_tmp/circle-icons/one-color/png/64px/tools.png')
+        self.toolBar.addAction(QtGui.QIcon(self.datamapIcon), "Datamaps")
+
+        self.returnsIcon = QtGui.QPixmap('/home/lemon/Downloads/icons_tmp/circle-icons/one-color/png/64px/upload.png')
+        self.toolBar.addAction(QtGui.QIcon(self.returnsIcon), "Returns")
+
+        self.templatesIcon = QtGui.QPixmap('/home/lemon/Downloads/icons_tmp/circle-icons/one-color/png/64px/dev.png')
+        self.toolBar.addAction(QtGui.QIcon(self.templatesIcon), "Templates")
+
+        self.importIcon = QtGui.QPixmap('/home/lemon/Downloads/icons_tmp/circle-icons/one-color/png/64px/arrow-down.png')
+        self.toolBar.addAction(QtGui.QIcon(self.importIcon), "Imports")
+
         self.actionTemplate_Manager.triggered.connect(
             self._openTemplateManagerWindow_slot)
 
