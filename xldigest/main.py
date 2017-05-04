3655fbd249ed81da752a93a76be64a37a7e4f41e
Matthew Lemon <lemon@matthewlemon.com>
2017-05-04 11:28:23 +0100

    starting to add functionality to detect missing database file and handle that

--- xldigest/main.py
+++ xldigest/main.py
@@ -7,11 +7,25 @@
 # WARNING! All changes made in this file will be lost!
 import sys
 
+from pathlib import Path
+
 from PyQt5 import QtWidgets, QtGui, QtCore
 
 import icons_rc
 
 from xldigest.widgets.main_ui import Ui_MainXldigestWindow
+from xldigest.widgets.base_import_wizard import BaseImportWizard
+from xldigest.database.setup import USER_DATA_DIR
+
+
+def test_db():
+    if Path(USER_DATA_DIR + '/db.sqlite').is_file():
+        return True
+    else:
+        return False
+
+
+DATABASE_PRESENT = test_db()
 
 
 class XldigestMainWindow(QtWidgets.QMainWindow, Ui_MainXldigestWindow):
@@ -38,6 +52,19 @@ class XldigestMainWindow(QtWidgets.QMainWindow, Ui_MainXldigestWindow):
 
         self.importIcon = QtGui.QPixmap(':/arrow-down.png')
         self.toolBar.addAction(QtGui.QIcon(self.importIcon), "Imports")
+
+        if not DATABASE_PRESENT:
+            self.overviewWidget.databasePresentLabel.setEnabled(True)
+            self.overviewWidget.baseSetupButton.setEnabled(True)
+            self.overviewWidget.baseSetupButton.clicked.connect(self._launch_wizard)
+
+    def _launch_wizard(self):
+        wizard = BaseImportWizard()
+        if wizard.exec_():
+            self.refreshUi()
+
+    def refreshUi(self):
+        pass
 
 
 def main():
