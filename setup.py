5c71f398d25095f68ca43bf8ae3acb28b81368ac
Matthew Lemon <lemon@matthewlemon.com>
2017-05-08 09:58:14 +0100

    another fix

--- setup.py
+++ setup.py
@@ -29,7 +29,8 @@ setup(
     py_modules=['icons_rc'],
     entry_points={
         'console_scripts':
-        ['xldigest-populate = xldigest.database.populate:main']
+        ['xldigest-populate = xldigest.database.populate:main',
+         'xldigest = xldigest.main: main']
     },
     install_requires=requirements,
     zip_safe=False,
