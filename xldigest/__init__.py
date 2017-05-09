4e92aa00d18862167c59c1185dfb4c535b4e1f6e
Matthew Lemon <lemon@matthewlemon.com>
2017-05-09 10:35:39 +0100

    put setup code in __init__ file

--- xldigest/__init__.py
+++ xldigest/__init__.py
@@ -1,1 +1,14 @@
+import os
+
+import appdirs
+
+APPNAME = 'xldigest'
+APPAUTHOR = 'MRLemon'
+USER_DATA_DIR = appdirs.user_data_dir(APPNAME, APPAUTHOR)
+USER_HOME = os.path.expanduser('~')
+DB_PATH = os.path.join(USER_DATA_DIR, 'db.sqlite')
+
+if not os.path.exists(USER_DATA_DIR):
+    os.makedirs(USER_DATA_DIR)
+
 from .startup import main_startup
