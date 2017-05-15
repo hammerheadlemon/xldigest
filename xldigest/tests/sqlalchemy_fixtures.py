7a0a4af30f15630efcc70cab48277dba3fc5f47c
Matthew Lemon <lemon@matthewlemon.com>
2017-05-15 20:40:48 +0100

    set up proper sqlalchemy fixtures and started to test the db

--- xldigest/tests/sqlalchemy_fixtures.py
+++ xldigest/tests/sqlalchemy_fixtures.py
@@ -5,16 +5,56 @@ from sqlalchemy.orm import sessionmaker
 import xldigest.database.models as models
 
 engine = create_engine("sqlite:///" + ':memory:')
+#engine = create_engine("sqlite:///" + '/tmp/new-test.sqlite')
 Session = sessionmaker(bind=engine)
 
 models.create_tables(engine)
 
-@pytest.fixture()
+
+@pytest.fixture(scope='module')
 def session() -> Session:
     session = Session()
+
     portfolio = models.Portfolio(name="Major Portfolio")
     session.add(portfolio)
+
+    series = models.Series(name="Financial Quarters")
+    session.add(series)
+
+    series_items = [models.SeriesItem(name=f"Q{x} 20{y}/{y + 1}",
+                                      series=1,
+                                      start_date=None,
+                                      end_date=None)
+                    for x in range(1, 5)
+                    for y in range(16, 21)
+                    ]
+    session.add_all(series_items)
+
+    projects = [models.Project(name=f"Project {x}")
+                for x in range(1, 34)]
+    session.add_all(projects)
+
+    datamap_items = [models.DatamapItem(
+        key=f"Datamap Key {x}",
+        bicc_sheet="Summary",
+        bicc_cellref=f"B{x}",
+        gmpp_sheet=f"GMPP Sheet",
+        gmpp_cellref=f"G{x}",
+        bicc_ver_form="")
+        for x in range(1, 101)]
+    session.add_all(datamap_items)
+
+    return_items = [[models.ReturnItem(
+        project_id=p,
+        series_item_id=1,
+        datamap_item_id=x,
+        value=f"Return Value {x}")
+        for x in range(1, 101)]
+        for p in range(1, 34)]
+
+    for r in return_items:
+        session.add_all(r)
+
     session.commit()
     yield session
     session.close()
-
--- xldigest/tests/test_db.py
+++ xldigest/tests/test_db.py
@@ -1,9 +1,14 @@
-from . sqlalchemy_fixtures import session
-from ..database.models import Portfolio
+from .sqlalchemy_fixtures import session
+from ..database.models import Portfolio, Project
 
-SESSION = session
+session = session
 
-def test_single_portfolio(SESSION):
-    assert SESSION.query(Portfolio.id).first()[0] == 1
-    assert SESSION.query(Portfolio.name).first()[0] == "Major Portfolio"
 
+def test_single_portfolio(session):
+    assert session.query(Portfolio.id).first()[0] == 1
+    assert session.query(Portfolio.name).first()[0] == "Major Portfolio"
+
+
+def test_single_project(session):
+    assert session.query(Project.name).first()[0] == "Project 1"
+    assert session.query(Project.name).all()[1][0] == "Project 2"
